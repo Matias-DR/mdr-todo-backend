947d744450e683dbd6ad154d58e854d65cbb98d8
Matias-DR <Matias-DR@outlook.com>
2024-01-27 17:24:36 -0300

    ADD: User and Task model

--- challenge/api/models.py
+++ challenge/api/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    '''
+        Entity/Model for the tasks
+
+        Attributes:
+            completed (bool): Determines if the user marked the task as completed.
+            description (str): User-entered descriptive colloquial text for the task.
+            title (str): Title of the task.
+            user (django.contrib.auth.models.User): Task owner.
+    '''
+
+    completed = models.BooleanField(default=False)
+    description = models.TextField()
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Title: {self.title}.\nUser: {self.user.__str__()}\n\n'
