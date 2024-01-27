a166265ff94c75049d01702350da7509daf61a3b
Matias-DR <Matias-DR@outlook.com>
2024-01-27 19:51:19 -0300

    ADD: JWT session manager

--- challenge/api/urls.py
+++ challenge/api/urls.py
@@ -7,6 +7,10 @@ from django.urls import (
     include
 )
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 router = DefaultRouter()
@@ -14,4 +18,19 @@ router = DefaultRouter()
 router.register(r'task', TaskViewSet, basename='task')
 router.register(r'user', UserViewSet, basename='user')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        '',
+        include(router.urls)
+    ),
+    path(
+        'token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+]
