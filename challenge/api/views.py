ce934476997eb7aee2fcabf7feb6bdd2fb4537b7
Matias-DR <Matias-DR@outlook.com>
2024-01-27 17:26:07 -0300

    ADD: user and task crud viewsets with ModelViewSet from rest_framework.models

--- challenge/api/views.py
+++ challenge/api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import (
+    User,
+    Task
+)
+from .serializers import (
+    UserSerializer,
+    TaskSerializer
+)
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
