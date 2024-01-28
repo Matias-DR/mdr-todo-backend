4b80c69f570731990a62c6002da4e9a760fcefee
Matias-DR <Matias-DR@outlook.com>
2024-01-28 17:53:25 -0300

    MOD: return types on functions and sorted imports

--- challenge/api/serializers.py
+++ challenge/api/serializers.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import ModelSerializer
+
 from .models import (
     User,
     Task
 )
-from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(ModelSerializer):
@@ -14,15 +15,25 @@ class UserSerializer(ModelSerializer):
             username (str): Username of the user.
     '''
 
-    def create(self, validated_data):
+    def create(
+        self,
+        validated_data: dict
+    ) -> User:
+        '''
+            Overrides the create method to hash the password.
+
+            Args:
+                validated_data (dict): Dictionary with the validated data.
+        '''
+
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
 
     class Meta:
         model = User
         fields = [
-            'username',
-            'password'
+            'password',
+            'username'
         ]
 
 
@@ -35,10 +46,11 @@ class TaskSerializer(ModelSerializer):
             description (str): User-entered descriptive colloquial text for the task.
             title (str): Title of the task.
     '''
+
     class Meta:
         model = Task
         fields = [
             'completed',
             'description',
-            'title'
+            'title',
         ]
