4aa65845f21e695658f9a193e070909dfc8b7925
Matias-DR <Matias-DR@outlook.com>
2024-01-28 17:53:58 -0300

    MOD: sorted imports and return types on functions

--- challenge/api/views.py
+++ challenge/api/views.py
@@ -1,3 +1,10 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.decorators import action
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+
 from .models import (
     User,
     Task
@@ -5,11 +12,6 @@ from .models import (
 from .serializers import (
     UserSerializer,
     TaskSerializer
-)
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import (
-    IsAuthenticated,
-    IsAdminUser
 )
 
 
@@ -38,6 +40,7 @@ class UserViewSet(ModelViewSet):
             Returns the user's queryset if authenticated and never that of all users.
             Only superusers can get the queryset of all users.
         '''
+
         if not self.request.user.is_superuser:
             return self.queryset.filter(pk=self.request.user.pk)
         return super().get_queryset()
@@ -57,19 +60,76 @@ class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter
+    ]
+    search_fields = [
+        'description',
+        'title'
+    ]
+    filterset_fields = [
+        'completed',
+        'created'
+    ]
 
     def get_queryset(self):
         '''
             Returns the task's queryset if authenticated and never that of all tasks.
             Only superusers can get the queryset of all task.
         '''
+
         if not self.request.user.is_superuser:
             return self.queryset.filter(user=self.request.user.pk)
         return super().get_queryset()
 
-    def perform_create(self, serializer):
+    def perform_create(self, serializer) -> None:
+        '''
+            Sets the user of the task to the user who created it.
+        '''
+
         serializer.save(user=self.request.user)
 
-    def mark_a_task_as_complete(self): ...
+    @action(
+        detail=True,
+        methods=[
+            'put',
+            'patch'
+        ]
+    )
+    def complete(
+        self,
+        request,
+        pk=None
+    ) -> Response:
+        '''
+            Allows to mark a task as complete.
+            It receive the task id, marks as complete and returns the task data.
+        '''
 
-    def mark_a_task_as_incomplete(self): ...
+        task = self.get_object()
+        serializer = self.get_serializer(task)
+        task.complete()
+        return Response(serializer.data, 200)
+
+    @action(
+        detail=True,
+        methods=[
+            'put',
+            'patch'
+        ]
+    )
+    def incomplete(
+        self,
+        request,
+        pk=None
+    ) -> Response:
+        '''
+            Allows to mark a task as incomplete.
+            It receive the task id, marks as complete and returns the task data.
+        '''
+
+        task = self.get_object()
+        serializer = self.get_serializer(task)
+        task.incomplete()
+        return Response(serializer.data, 200)
