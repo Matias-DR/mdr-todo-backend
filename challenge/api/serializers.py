f77cfee2aa4ca5da6551ad2a0a26d0d7167e6b4f
Matias-DR <Matias-DR@outlook.com>
2024-02-13 19:09:32 -0300

    ADD: email in meta class for user serializer & custon token serializer

--- challenge/api/serializers.py
+++ challenge/api/serializers.py
@@ -2,6 +2,7 @@ import logging
 
 from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from .models import (
     User,
@@ -31,7 +32,8 @@ class UserSerializer(ModelSerializer):
                 validated_data (dict): Dictionary with the validated data.
         '''
 
-        logger.info(f'UserSerializer create -> User {validated_data["username"]} created.')
+        logger.info(
+            f'UserSerializer create -> User {validated_data["username"]} created.')
         validated_data['password'] = make_password(validated_data['password'])
         return super().create(validated_data)
 
@@ -40,7 +42,8 @@ class UserSerializer(ModelSerializer):
         fields = [
             'pk',
             'password',
-            'username'
+            'username',
+            'email'
         ]
 
 
@@ -63,3 +66,29 @@ class TaskSerializer(ModelSerializer):
             'title',
             'created'
         ]
+
+
+class TokenSerializer(TokenObtainPairSerializer):
+    '''
+        Custom token serializer for the JWT token.
+    '''
+
+    @classmethod
+    def get_token(cls, user):
+        '''
+            Adds pk, username and email fields of the current user to the super
+            get_token method.
+
+            Args:
+                user (api.models.User): User to get the token for.
+        '''
+
+        logger.info(
+            f'TokenSerializer get_token -> Token for user {user.username}.'
+        )
+        token = super().get_token(user)
+        token['pk'] = user.pk
+        token['username'] = user.get_username()
+        token['email'] = user.get_email_field_name()
+        logger.info(f'ESTO QUEDA COMO TOKEN EN EL SERIALIZER {token}')
+        return token
