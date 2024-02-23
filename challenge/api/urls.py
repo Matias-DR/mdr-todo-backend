d5258ce2efb0f9133ec98ad46f52e381dd4b5266
Matias-DR <Matias-DR@outlook.com>
2024-02-22 23:47:39 -0300

    MOD: document formated

--- challenge/api/urls.py
+++ challenge/api/urls.py
@@ -1,64 +1,30 @@
-from .views import (
-    TaskViewSet,
-    UserViewSet
-)
-from django.urls import (
-    path,
-    include
-)
+from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
-    SpectacularSwaggerView
+    SpectacularSwaggerView,
 )
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import TaskViewSet, UserViewSet
 
 router = DefaultRouter()
 
-router.register(
-    r'task',
-    TaskViewSet,
-    basename='task'
-)
-router.register(
-    r'user',
-    UserViewSet,
-    basename='user'
-)
+router.register(r"task", TaskViewSet, basename="task")
+router.register(r"user", UserViewSet, basename="user")
 
 urlpatterns = [
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        '',
-        include(router.urls)
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
     path(
-        'token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
+        "schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
     ),
-    path(
-        'token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
-    path(
-        'schema/',
-        SpectacularAPIView.as_view(),
-        name='schema'
-    ),
-    path(
-        'schema/swagger-ui/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui'
-    ),
-    path(
-        'schema/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc'
-    )
 ]
